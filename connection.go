4a72eeef841e2ef4cce0d6726db63f48d86973d8
Hamza <hammoti@gmail.com>
2020-06-06 21:13:13 -0600

    connection

---
+++ connection.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"github.com/hamzam15comp/vertex"
+
+)
+
+func main() {
+
+	lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
+	lsOut, err := lsCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> ls -a -l -h")
+	fmt.Println(string(lsOut))
+}
---
+++ test.go
@@ -0,0 +1,12 @@
+package main
+
+import(
+	"fmt"
+
+	"github.com/hamzam15comp/vertex"
+
+)
+
+func main() {
+
+	datatype, data, err := exec.ReadData()
